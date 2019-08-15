d2735f66295f6581f311206ed62a8d634eb5caec
calmdownkarm <karmanyaaggarwal@gmail.com>
2019-08-15 10:32:53 +0530

    word replacements step added

--- preprocess.py
+++ preprocess.py
@@ -6,6 +6,19 @@ Handles Preprocessing Tags
 """
 
 
+def collide_words(list_of_strings):
+    """
+    Takes a list of tokens and replaces them with replacements
+    :param list_of_strings: list of strings, each string is a token.
+    :return: Returns list of tokens.
+    """
+    replacements = {}
+    return [
+        replacements[token] if token in replacements.keys() else token
+        for token in list_of_strings
+    ]
+
+
 def create_user_tags(instring):
     """
     Masks a string with `xxUSER` tags for Twitter-style mentions, IP Addresses, <USER> tags.
@@ -30,7 +43,7 @@ def preprocess(strings, **kwargs):
         rm_useless_spaces,
         create_user_tags,
     ]
-    text_post_rules = [replace_all_caps, deal_caps]
+    text_post_rules = [replace_all_caps, deal_caps, collide_words]
     tokenizer = Tokenizer(
         pre_rules=text_pre_rules, post_rules=text_post_rules, **kwargs
     )
